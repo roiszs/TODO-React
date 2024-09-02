14a13607579714b966d551a78d54a49b9258ad36
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-09-02 13:25:17 -0500

    Estilos al titulo

--- src/Todotitulo.js
+++ src/Todotitulo.js
@@ -2,8 +2,8 @@ import './Todotitulo.css';
 
 function Todotitulo({total, completed}) {
     return (
-      <h1>
-        Has completado {total} de {completed} TODOS
+      <h1 className='titulo'>
+        Has completado <span> {total} </span> de <span>{completed}</span> TODOS
       </h1>
     );
   }
