4a04cca2a8d13c7b8aab8acb2fbe93d14dfde1d8
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-09-02 13:49:16 -0500

    Editando los items

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,28 @@
+import './Todoitem.css'
+
 
 function TodoItem(props) {
     return (
-      <li>
-        <span>✅ {props.completed}</span>
-        <p>{props.text}</p> 
-        <span>❌</span>
+      <li className='TodoItem'>
+        <span className='Icon Icon-check Icon-check--active'>
+
+          ✅ 
+          {props.completed}
+
+        </span>
+
+
+        <p className='TodoItem-p TodoItem-p--complete'>
+          {props.text}
+          </p> 
+
+
+        <span className='Icon Icon-delete'>
+          ❌
+          </span>
       </li>
     );
   }
+  
 
   export {TodoItem}
