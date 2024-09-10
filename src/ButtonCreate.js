fe70ca41837a50271875dd233a2ac6d8fda486ff
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-09-10 13:52:27 -0500

    Clases del estado de react

--- src/ButtonCreate.js
+++ src/ButtonCreate.js
@@ -2,7 +2,13 @@ import './Button.css';
 
 function ButtonCreate() {
     return (
-      <button className="Boton">
+      <button className="Boton" 
+      onClick={(event) => {
+      console.log('Le diste click')
+      console.log(event)
+      console.log(event.target)
+      }
+      }>
         +
       </button>
     );
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import './TodoSearch.css'
 
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+
     return (
      <input className="todo-search
-     "placeholder="Cortar Cabello"/>
+     "placeholder="Cortar Cabello"
+      value={searchValue}
+     onChange={(event) => {
+      setSearchValue(event.target.value)
+     }}/>
     );
   }
 
