813a281ab5997a1a744d88fad34f1d17a7960aa4
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-09-20 10:18:28 -0500

    Editando los iconos de la aplicacion

--- src/Todo.Icon.js
+++ src/Todo.Icon.js
@@ -2,11 +2,11 @@ import { FaCheckCircle } from "react-icons/fa";
 import { TiDelete } from "react-icons/ti";
 
 const iconTypes = {
-    'check': <FaCheckCircle />,
-    'delete':  <TiDelete />,
-}
+    "check": <FaCheckCircle />,
+    "delete":  <TiDelete />,
+};
 
-function TodoIcon (type) {
+function TodoIcon ({type}) {
     return (
         <span className={`Icon Icon-svg Icon-${type}`}>
             
